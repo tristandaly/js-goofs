5e1683d9e6eace4a2b9497d12ca756a0e1a6fc19
Tristan-Daly <tristan.daly@ibm.com>
2019-10-09 14:48:31 -0500

    Began work on code to emulate Contact Signal

---
+++ contact.js
@@ -0,0 +1,25 @@
+function isPrime(number) {
+    if (number == 0 || number == 1) return false;
+    let root = Math.floor(Math.sqrt(number));
+    for (let i = 2; i <= root; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+for(let i = 0; i < 100; i++) {
+  if (isPrime(i) === true) {
+    setTimeout(1000)
+    signal = i;
+    for(let j = 0; j < i; j++) {
+      console.log("Hello World", i);
+      process.nextTick();
+    }
+  }
+}  
+
+//console.log(isPrime(11)); //true
+//console.log(isPrime(36)); //false
